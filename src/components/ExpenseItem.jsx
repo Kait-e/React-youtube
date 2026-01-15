04ed50fb5ad46824520a9090695744154949df5e
Kait-e <kait-evander.uusimaa@voco.ee>
2026-01-15 15:03:00 +0200

    ylesanne 1 tehtud

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,19 +1,11 @@
+import ExpenseDate from './Expensedate'
 import './ExpenseItem.css'
 
 const ExpenseItem = (props) => {
-    
-    const day = props.data.date.toLocaleString('en-US', {day: '2-digit'})
-    const month = props.data.date.toLocaleString('en-US', {month: 'long'})
-    const year = props.data.date.getFullYear()
 
-    
     return  (
         <div className="expense-item">
-            <div>
-            <div>{month}</div>
-            <div>{day}</div>
-            <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.data.date}/>   
             <div className= "expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">${props.data.price}</div>
