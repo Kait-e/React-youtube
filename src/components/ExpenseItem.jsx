e54797c2f5220294c6e95f1e61baaea2969657a1
Kait-e <Kait-evander.uusimaa@voco.ee>
2026-02-02 09:56:46 +0200

    ylesanne 3 tehtud

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,24 +1,18 @@
 import ExpenseDate from './Expensedate'
 import './ExpenseItem.css'
+import Card from "./card.jsx"
 
 const ExpenseItem = (props) => {
 
     return  (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpenseDate date={props.data.date}/>   
             <div className= "expense-item__description">
                 <h2>{props.data.title}</h2>
                 <div className="expense-item__price">${props.data.price}</div>
             </div>
-        </div>
+        </Card>
     )
-
-  return (
-      <div className="App">
-        <ExpenseItem data = {expenses[0]}/>
-        <ExpenseItem data = {expenses[1]}/>
-      </div>
-    );
 }
 
 export default ExpenseItem
